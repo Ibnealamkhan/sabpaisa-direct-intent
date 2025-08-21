aace8624f1d6baff825b1b2a807764cb1f8d019c
Ibnealamkhan <52732277+Ibnealamkhan@users.noreply.github.com>
2025-08-22 02:29:48 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,11 +5,3 @@ $username   = "username";
 $password   = "password";
 $authKey    = "authkey";
 $authIV     = "authiv";
-/*
-include_once 'src/sabpaisa.php';
-$clientCode = "BILL99";
-$username   = "billhubapp@gmail.com";
-$password   = "BILL99_SP19103";
-$authKey    = "JRIipyvApRUKxLLH";
-$authIV     = "Cr3ez7MGY4tD79TO";
-*/
